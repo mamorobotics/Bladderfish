945f9478c88d2e2864aab5729cbbfd6cd7f193ad
Mustafa Khan <74122733+MustafaKhan0@users.noreply.github.com>
2026-05-17 17:02:04 -0400

    wifi float stuff

--- FloatTesting/FloatTesting.ino
+++ FloatTesting/FloatTesting.ino
@@ -1,221 +1,304 @@
+#include <WiFiNINA.h>
+#include <utility/wifi_drv.h>
 #include <Wire.h>
 #include "Adafruit_MPRLS.h"
 #include <RTCZero.h>
 
-#include <WiFiNINA.h>
-#include <utility/wifi_drv.h>
+// ================== WIFI ==================
+char ssid[] = "midnightRobotics";
+char pass[] = "hughhughhughhugh";
 
-#include <SPI.h>
-#include <RH_RF95.h>
+WiFiServer server(80);
+int status = WL_IDLE_STATUS;
 
-#include <iostream>
-#include <string>
-#include <cstring>
-
+// ================== HARDWARE ==================
 #define ENABLE_MOTOR 2
 #define MOTORA1 3
 #define MOTORA2 5
 
-#define RFM95_CS 1
-#define RFM95_RST 0
-#define RFM95_INT 4
-#define RF95_FREQ 433.0
-
-RH_RF95 rf95(RFM95_CS, RFM95_INT);
-
 RTCZero rtc;
-
 
 #define RESET_PIN  -1 
 #define EOC_PIN    -1 
 Adafruit_MPRLS mpr = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
 
-char TEAM_ID[7] = "TN9999";
+// ================== STATE ==================
+int state = 0;
+// 0 = idle
+// 1 = vertical profile
+// 2 = send data
+// 3 = fill neutral
+// 4 = test reply
+// 5 = pump in 5 seconds
+// 6 = pump out 5 seconds
 
-int state = 0; 
+// ================== LOGGING ==================
+char TEAM_ID[7] = "TN9999";
 
 int packetTime[10][2];
 int packetDepth[10];
 int packetCounter = 0;
 
+// ================== NON-BLOCKING PUMP ==================
+bool pumpRunning = false;
+bool pumpDirectionIn = true;
+unsigned long pumpStartTime = 0;
+unsigned long pumpDuration = 0;
 
-int pumpPin = 5;
+// ================== PROFILE CONTROL ==================
+int profileStep = 0;
+unsigned long stepStartTime = 0;
 
-bool good = true;
-
-// 0: waiting for command to do a vertical profile
-// 1: doing a vertical profile
-// 2: sending data packets after vertical profile
-
+// =====================================================
 
 void setup() {
-  // put your setup code here, to run once:
-  
+  // Serial.begin(9600);
+  // while (!Serial);
 
-  Serial.begin(9600);
-  while (!Serial) { }  // if using native USB boards
-  Serial.println("BOOT"); 
-
-  // Radio:
-
- 
-  setupRadio();
-  Serial.println("Radio setup!");
-
-  // Motor:
-
+  // Motor
   pinMode(ENABLE_MOTOR, OUTPUT);
   pinMode(MOTORA1, OUTPUT);
   pinMode(MOTORA2, OUTPUT);
 
-  digitalWrite(ENABLE_MOTOR, LOW);
+  // RGB LED
+  WiFiDrv::pinMode(25, OUTPUT);
+  WiFiDrv::pinMode(26, OUTPUT);
+  WiFiDrv::pinMode(27, OUTPUT);
 
-
-  WiFiDrv::pinMode(25, OUTPUT); //define GREEN LED
-  WiFiDrv::pinMode(26, OUTPUT); //define RED LED
-  WiFiDrv::pinMode(27, OUTPUT); //define BLUE LED
-
-  Serial.println("Motor Setup");
-
-
-  // Clock:
-
+  // RTC
   rtc.begin();
-
   rtc.setHours(0);
   rtc.setMinutes(0);
   rtc.setSeconds(0);
 
-  Serial.println("Clock Setup");
-
-  // Pressure sensor: 
-
-  if (! mpr.begin()) {
-    Serial.println("Failed to communicate with MPRLS sensor, check wiring?");
-    good = false;
-    while (1) {
-      delay(10);
-    }
-  }
-
-  good = true;
-  Serial.println("Found MPRLS sensor");
-
-  if (good) {
-    WiFiDrv::analogWrite(25, 255); //GREEN
-    WiFiDrv::analogWrite(26, 0);   //RED
-    WiFiDrv::analogWrite(27, 0);   //BLUE
-  }
-
-  else{
-    WiFiDrv::analogWrite(25, 0); //GREEN
-    WiFiDrv::analogWrite(26, 255);   //RED
-    WiFiDrv::analogWrite(27, 0);   //BLUE
-  }
-  
-
-}
-
-
-void setupRadio() {
-  pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
-
-  delay(100);
-
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
-
-
-  while (!rf95.init()) {
-    Serial.println("LoRa radio init failed"); 
-    good = false;
+  // Pressure sensor
+  if (!mpr.begin()) {
+    setLED(255, 0, 0); // red = failure
     while (1);
   }
-  good = true;
-  Serial.println("LoRa radio init OK!");
+  setLED(0, 255, 0); // green = OK
 
-
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
-    good = false;
-    while (1);
+  // WiFi AP
+  status = WiFi.beginAP(ssid, pass);
+  if (status != WL_AP_LISTENING) {
+    while (true);
   }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
-  good = true;
-  
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
-  // you can set transmitter powers from 5 to 23 dBm:
-  rf95.setTxPower(23, false);
+  delay(3000);
+  server.begin();
+
+  Serial.println("System Ready");
 }
+
+// =====================================================
+// MAIN LOOP
+// =====================================================
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+  handleWiFi();
 
-  if (state == 0) {
-    waitCommand();
-    delay(500);
-  }
-  if (state == 1) {
-    verticalProfile();
-    state++;
-    Serial.println("Profile Done");
-  }
-  if (state == 2) {
-    Serial.println("Sending data");
-    sendData();
-    state = 0;
-  }
-  if (state == 3) {
-    fillNeutral();
-    state = 0;
-  }
-  if (state == 4) {
-    sendReplyPacket();
-    state = 0;
-  }
+  updatePump();
+
+  switch (state) {
+    case 1:
+      runVerticalProfile();
+      break;
+
+    case 2:
+      sendData();
+      state = 0;
+      break;
+
+    case 3:
+      startPump(85000, true);
+      setLED(0, 0, 255);
+      state = 0;
+      break;
+
+    case 4:
+      sendReply();
+      state = 0;
+      break;
+
+    case 5:
+      if (!pumpRunning) {
+        startPump(5000, true);
+        setLED(0, 255, 255);
+      }
+      state = 0;
+      break;
+
+    case 6:
+      if (!pumpRunning) {
+        startPump(5000, false);
+        setLED(255, 255, 0);
+      }
+      state = 0;
+      break;
+      }
 }
 
+// =====================================================
+// WIFI CONTROL
+// =====================================================
 
-void waitCommand() {
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
-  if (rf95.waitAvailableTimeout(1000)) {
-    if (rf95.recv(buf, &len))
-    {
-      Serial.print("Recieved: ");
-      Serial.println((char*)buf);
+void handleWiFi() {
+  WiFiClient client = server.available();
 
-      if (strcmp((char*)buf, "start") == 0) {
-        state = 1; // begin vertical profile
+  if (!client) return;
+
+  String line = "";
+
+  while (client.connected()) {
+    if (client.available()) {
+      char c = client.read();
+
+      if (c == '\n') {
+
+        if (line.length() == 0) {
+
+          // ===== RESPONSE =====
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+
+          client.println("<html><head>");
+          client.println("<meta name='viewport' content='width=device-width, initial-scale=1'>");
+          client.println("</head><body style='text-align:center;'>");
+
+          client.println("<h1>ROV Control</h1>");
+
+          client.println("<a href='/start'><button style='width:80%;height:60px;font-size:20px;'>Start Profile</button></a><br><br>");
+          client.println("<a href='/fill'><button style='width:80%;height:60px;font-size:20px;'>Fill Neutral</button></a><br><br>");
+          client.println("<a href='/test'><button style='width:80%;height:60px;font-size:20px;'>Test</button></a><br><br>");
+          client.println("<a href='/in5'><button style='width:80%;height:60px;font-size:20px;'>Pump IN (5s)</button></a><br><br>");
+          client.println("<a href='/out5'><button style='width:80%;height:60px;font-size:20px;'>Pump OUT (5s)</button></a><br><br>");
+
+          client.print("<p>Depth: ");
+          client.print(readPressure());
+          client.println("</p>");
+
+          client.println("</body></html>");
+          break;
+        } else {
+          line = "";
+        }
       }
-      if (strcmp((char*)buf, "fill") == 0) {
-        state = 3; // begin vertical profile
-      }
-      if (strcmp((char*)buf, "test") == 0) {
-        state = 4; // begin vertical profile
+      else if (c != '\r') {
+        line += c;
       }
 
-      
-
-
-      Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);    
+      // ===== COMMANDS =====
+      if (line.endsWith("GET /start")) state = 1;
+      if (line.endsWith("GET /fill"))  state = 3;
+      if (line.endsWith("GET /test"))  state = 4;
+      if (line.endsWith("GET /in5"))  state = 5;
+      if (line.endsWith("GET /out5")) state = 6;
     }
   }
+
+  client.stop();
 }
 
-float readPressure(){
-  float pressure_hPa = mpr.readPressure();
-  return pressure_hPa;
+// =====================================================
+// PUMP CONTROL (NON-BLOCKING)
+// =====================================================
+
+void startPump(unsigned long duration, bool in) {
+  pumpRunning = true;
+  pumpDirectionIn = in;
+  pumpDuration = duration;
+  pumpStartTime = millis();
+
+  digitalWrite(ENABLE_MOTOR, HIGH);
+
+  if (in) {
+    digitalWrite(MOTORA1, LOW);
+    digitalWrite(MOTORA2, HIGH);
+  } else {
+    digitalWrite(MOTORA1, HIGH);
+    digitalWrite(MOTORA2, LOW);
+  }
 }
 
+void updatePump() {
+  if (!pumpRunning) return;
+
+  if (millis() - pumpStartTime >= pumpDuration) {
+    digitalWrite(ENABLE_MOTOR, LOW);
+    pumpRunning = false;
+  }
+}
+
+// =====================================================
+// VERTICAL PROFILE (NON-BLOCKING)
+// =====================================================
+
+void runVerticalProfile() {
+
+  if (pumpRunning) return; // wait for pump to finish
+
+  switch (profileStep) {
+
+    case 0:
+      Serial.println("Start Profile");
+      setLED(255, 0, 0);
+      startPump(5000, true);
+      savePacket();
+      profileStep++;
+      break;
+
+    case 1:
+      startPump(5000, true);
+      savePacket();
+      profileStep++;
+      break;
+
+    case 2:
+      stepStartTime = millis();
+      profileStep++;
+      break;
+
+    case 3:
+      if (millis() - stepStartTime > 5000) {
+        savePacket();
+        profileStep++;
+      }
+      break;
+
+    case 4:
+      startPump(5000, false);
+      savePacket();
+      profileStep++;
+      break;
+
+    case 5:
+      stepStartTime = millis();
+      profileStep++;
+      break;
+
+    case 6:
+      if (millis() - stepStartTime > 5000) {
+        savePacket();
+        profileStep++;
+      }
+      break;
+
+    case 7:
+      state = 2;
+      profileStep = 0;
+      Serial.println("Profile Done");
+      break;
+  }
+}
+
+// =====================================================
+// DATA + SENSORS
+// =====================================================
+
+float readPressure() {
+  return mpr.readPressure();
+}
 
 void savePacket() {
   packetTime[packetCounter][0] = rtc.getMinutes();
@@ -224,117 +307,36 @@ void savePacket() {
   packetCounter++;
 }
 
+// =====================================================
+// OUTPUT (SERIAL INSTEAD OF LORA)
+// =====================================================
 
-void fillNeutral() {
-  pump(85000, true);
-  WiFiDrv::analogWrite(25, 0); //GREEN
-  WiFiDrv::analogWrite(26, 0);   //RED
-  WiFiDrv::analogWrite(27, 255);   //BLUE
-}
-
-void sendReplyPacket() {
-  char msg[64];
-    snprintf(msg, sizeof(msg), "TEST_%d:%d",
-        rtc.getMinutes(), rtc.getSeconds());
-
-    Serial.println(msg);
-    Serial.println(strlen(msg));
-
-    rf95.send((uint8_t *)msg, strlen(msg));
-    rf95.waitPacketSent();
-}
-
-void verticalProfile() {
-  // fill the bladder
-  Serial.println("Vertical Profile Start");
-  WiFiDrv::analogWrite(25, 0); //GREEN
-  WiFiDrv::analogWrite(26, 255);   //RED
-  WiFiDrv::analogWrite(27, 0);   //BLUE
-
-  pump(5000, true);
-  savePacket();
-  pump(5000, true);
-  savePacket();
-
-  
-
-  delay(5000);
-  savePacket();
-
-  // neutral
-
-  
-  Serial.println("Neutral");
-  pump(5000, false);
-  savePacket();
-  
-
-  // wait 10 seconds
-
-  delay(5000);
-  savePacket();
-  delay(5000);
-  savePacket();
-
-  // empty the bladder
-  Serial.println("Float");
-  pump(5000, false);
-  savePacket();
-
-}
-
-
-void pump(int ms, bool in) {
-  // turn on pump for in or out
-
-  digitalWrite(ENABLE_MOTOR, HIGH);
-
-  if (in) {
-    digitalWrite(MOTORA1, LOW);
-    digitalWrite(MOTORA2, HIGH);
-  }
-
-  else {
-    digitalWrite(MOTORA1, HIGH);
-    digitalWrite(MOTORA2, LOW);
-  }
-
-  delay(ms);
-
-  // turn off pump
-  digitalWrite(ENABLE_MOTOR, LOW);
+void sendReply() {
+  Serial.print("TEST_");
+  Serial.print(rtc.getMinutes());
+  Serial.print(":");
+  Serial.println(rtc.getSeconds());
 }
 
 void sendData() {
   for (int i = 0; i < packetCounter; i++) {
-    
-    // char msg[32] = {0};
-    // char packetStuff[3][8];
-
-
-    // strcat(msg, TEAM_ID);
-    // strcat(msg, "_");
-    // itoa(packetTime[i][0], packetStuff[0], 10);
-    // itoa(packetTime[i][1], packetStuff[1], 10);
-    // itoa(packetDepth[i], packetStuff[2], 10);
-    // strcat(msg, packetStuff[0]);
-    // strcat(msg, ":");
-    // strcat(msg, packetStuff[1]);
-    // strcat(msg, "_");
-    // strcat(msg, packetStuff[2]);
-
-    char msg[64];
-    snprintf(msg, sizeof(msg), "%s_%d:%d_%d",
-         TEAM_ID, packetTime[i][0], packetTime[i][1], packetDepth[i]);
-
-    Serial.println(msg);
-    Serial.println(strlen(msg));
-
-    rf95.send((uint8_t *)msg, strlen(msg));
-    rf95.waitPacketSent();
-    Serial.println("Sent packet");
-    delay(300);
+    Serial.print(TEAM_ID);
+    Serial.print("_");
+    Serial.print(packetTime[i][0]);
+    Serial.print(":");
+    Serial.print(packetTime[i][1]);
+    Serial.print("_");
+    Serial.println(packetDepth[i]);
   }
-
   packetCounter = 0;
+}
+
+// =====================================================
+// LED
+// =====================================================
+
+void setLED(int r, int g, int b) {
+  WiFiDrv::analogWrite(25, g);
+  WiFiDrv::analogWrite(26, r);
+  WiFiDrv::analogWrite(27, b);
 }
